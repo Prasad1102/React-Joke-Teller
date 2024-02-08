6ab4516b94936844315a06ccf969fa751d6cc5c5
Prasad Shelke <shelkeprasad1102@gmail.com>
2024-02-08 14:20:16 +0530

    first commit to main branch

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import JokeTeller from "./Components/JokeTeller";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <JokeTeller />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/JokeTeller.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import style from "./JokeTeller.module.css";
+
+const JokeTeller = () => {
+  const [joke, setJoke] = useState("");
+  const fetchJoke = async () => {
+    const response = await fetch("https://v2.jokeapi.dev/joke/Any?type=single");
+    const data = await response.json();
+    setJoke(data.joke);
+    tellJoke(data.joke);
+  };
+
+  const tellJoke = (joke) => {
+    const speech = new SpeechSynthesisUtterance();
+    speech.text = joke;
+    window.speechSynthesis.speak(speech);
+  };
+
+  return (
+    <div>
+      <h2 className={style.heading}>Joke Teller</h2>
+      <button onClick={fetchJoke} className="btn btn-primary">
+        Tell Me a Joke
+      </button>
+      <div className="container">{joke && <p>{joke}</p>}</div>
+    </div>
+  );
+};
+
+export default JokeTeller;
