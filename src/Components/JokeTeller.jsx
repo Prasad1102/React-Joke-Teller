ff3ab732a2c521c06fb33cc7b80d02b35c237d1b
Prasad Shelke <shelkeprasad1102@gmail.com>
2024-02-12 22:44:02 +0530

    i dont know commit

--- src/Components/JokeTeller.jsx
+++ src/Components/JokeTeller.jsx
@@ -25,6 +25,9 @@ const JokeTeller = () => {
       <button onClick={fetchJoke} className="btn btn-primary">
         Joke
       </button>
+      <button onClick={fetchJoke} className="btn btn-primary">
+        Joke
+      </button>
       <div className="container">{joke && <p>{joke}</p>}</div>
     </div>
   );
