6f8ab1746f42415226534787a66600c5a058a5a3
Prasad Shelke <shelkeprasad1102@gmail.com>
2024-02-13 16:29:36 +0530

    Commit TryIt From My React Side

--- src/Components/TryItComponent.jsx
+++ src/Components/TryItComponent.jsx
@@ -1,5 +1,10 @@
 const TryItComponent = () => {
-  return <></>;
+  return (
+    <>
+      <p>This is a component from My-React-project</p>
+      <h3>Hello Sir..! Good Morning</h3>
+    </>
+  );
 };
 
 export default TryItComponent;
