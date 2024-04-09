6ca62dfa94dd770ef98ab1419492a228ceb2edcf
Prasad Shelke <99793793+Prasad1102@users.noreply.github.com>
2024-04-09 09:18:28 +0530

    Update TryItComponent.jsx
    Unwanted Text Removed

--- src/Components/TryItComponent.jsx
+++ src/Components/TryItComponent.jsx
@@ -2,7 +2,6 @@ const TryItComponent = () => {
   console.log("TryItComponent");
   return (
     <>
-      <p>This is a component from My-React-project</p>
       <h3>Hello Sir..!</h3>
     </>
   );
